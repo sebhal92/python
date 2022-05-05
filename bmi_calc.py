247551d947bb09aa82f79225fb47b0587722c111
sebhal92 <58229006+sebhal92@users.noreply.github.com>
2022-05-05 17:08:07 +0200

    Add files via upload

---
+++ bmi_calc.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+wzrost=float(input('jaki masz wzrost? '))
+waga=float(input('ile wazysz? '))
+bmi=(waga/(wzrost**2))
+print('twoje bmi wynosi:',bmi)
---
+++ proste_pytania.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+imie=input('jak masz na imie?')
+print('czeœæ',imie)
+print('ile masz lat?')
+wiek=int(input())
+print('z którego peronu odjecha³ harry potter?')
+peron=input()
+print(imie,'jestes za stary, masz juz',wiek,'lat. nie pojedziesz do hogwartu z peronu',peron)
