18fe9d4c01f2ca819cc6ed38f1facb24514a4f98
sebhal92 <58229006+sebhal92@users.noreply.github.com>
2022-05-09 21:57:29 +0200

    Update jednostki.py

--- jednostki.py
+++ jednostki.py
@@ -6,6 +6,7 @@ print("waga = {} kilogramow".format(kilogram))
 metr=centymetr/100
 cal=centymetr/2.54
 funt=kilogram*2.2046
+#{:6.4f} - 6 znakow na zapisanie liczby, z czego 4 beda po przecinku
 print("dlugosc w calach wynosi {:6.4f}" .format(metr))
 print("dlugosc w calach wynosi {:6.4f}" .format(cal))
 print("waga w funtach wynosi {:6.4f}" .format(funt))
