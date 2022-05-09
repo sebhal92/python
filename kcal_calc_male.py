5165bf22a931497da4a2988adb2fa1a37ed1cc8d
sebhal92 <58229006+sebhal92@users.noreply.github.com>
2022-05-09 21:29:43 +0200

    Add files via upload

---
+++ kcal_calc_male.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+wzrost=int(input('jaki masz wzrost w centymetrach? '))
+waga=float(input('ile wazysz? '))
+wiek=int(input('ile masz lat? ' ))
+mnoznik=1.4
+S=5
+zapotrzebowanie=10*waga+6.25*wzrost-5*wiek+S
+print('twoje zapotrzebowanie kaloryczne wynosi: ',zapotrzebowanie)
