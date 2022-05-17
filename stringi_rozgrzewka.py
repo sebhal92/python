b93253e1e1a80922eb3f8a7c6fe82fa3bc9ff103
sebhal92 <58229006+sebhal92@users.noreply.github.com>
2022-05-17 17:43:09 +0200

    Add files via upload

---
+++ stringi_rozgrzewka.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+sentence = "Kurs Pythona jest prosty i przyjemny"
+len(sentence)
+sentence[18:24]
+sentence[7]
+sentence[12]
+sentence[-4]
+sentence[37]
+sentence=sentence[0]+'ó'+sentence[2:] 
+sentence=sentence[0:27]+sentence[27]+'s'+sentence[29:]
+sentence
